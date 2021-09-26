0ed45e933df9c03d3a4a57ca95905bbaafe1fc38
寺坂綾香 <1219noppoppo@gmail.com>
2021-09-26 22:30:44 +0900

    [Update]時間をリアルタイムに表示

--- Date/today.js
+++ Date/today.js
@@ -1,13 +1,17 @@
-var today = new Date();
+function myFunc(){
+  var today = new Date();
 
-var year = today.getFullYear();
-var month = today.getMonth() + 1;
-var day = today.getDate();
-var week = today.getDay();
-var hour = today.getHours();
-var minute = today.getMinutes();
-var second = today.getSeconds();
+  var year = today.getFullYear();
+  var month = today.getMonth() + 1;
+  var day = today.getDate();
+  var week = today.getDay();
+  var hour = today.getHours();
+  var minute = today.getMinutes();
+  var second = today.getSeconds();
 
-var yobi = new Array("日", "月", "火", "水", "木", "金", "土");
+  var yobi = new Array("日", "月", "火", "水", "木", "金", "土");
+  var time = year + "年" + month + "月" + day + "日" + "(" + yobi[week] + ")" + hour + "時" + minute + "分" + second + "秒";
+  document.Form.FormTime.value = time;
+}
 
-document.write(year + "年" + month + "月" + day + "日" + "(" + yobi[week] + ")" + hour + "時" + minute + "分" + second + "秒");
+setInterval("myFunc()",1000);
